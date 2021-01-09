851b40f1d59acb676f7a095858063aea66f02b22
Kimberleyckl <kimberleyckl@gmail.com>
2021-01-09 21:21:42 +1300

    mod_147

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import Aux from '../../hoc/Auxiliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.module.css';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
 
 const layout = (props) => (
     <Aux>
-        <Toolbar/>
+        <Toolbar />
+        <SideDrawer />
         <div>toolbar, side drawer, backdrop</div>
         <main className = {classes.Content}>
             {props.children}
